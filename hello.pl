cb6aa5eb23e03bd61ee7a4f8dd3d985d9a3d206b
alexd <alexd@z-inc.net>
2016-05-25 12:38:41 +0500

    .

--- hello.pl
+++ hello.pl
@@ -13,8 +13,29 @@ if (not defined $filename) {
 
 print "filename: $filename\n";
 
+open(FILE, $filename) or die "Cannot open file $filename\n";
+
 my $count = 0;
-while( <$filename> ) { $count++; }
-print "line in file: $count\n";
+my $countLinesStartFromDigit = 0;
+while( my $line = <FILE> ) {
+    if ($line =~ /^\d/) {
+        $countLinesStartFromDigit++;
+    }
+    $count++;
+}
+print "count lines: $count\n";
+print "count lines start from digit: $countLinesStartFromDigit\n";
 
 
+#------------------------
+#читаем файл построчно,
+# если эта строка не начинается с '\tat ' и не начинается с цифры (что говорит что это не Stack Trace, a просто log)
+# тогда значит что эта строчка это начало нового Stack Trace
+# и все следующие строчки начинающиеся с '\tat ' это часть этого Stack Trace
+#
+# Нам нужно прочитать целиком один Stack Trace и занести его в память
+#
+# каждому СтекТрейсу мы должны поставить в соответствие массив чисел - номера строк в исходном файле где этот СтекТрейс был встречен
+# сам по себе СтекТрейс это должен быть массив строк
+
+# на груви подошла бы такая структура: список мап, где каждая мапа: [stackTrace: списокСтрокЭтогоСтекстрейса, lines: списокЧиселСНомерамиСтокГдеЭтотСтекТрейсБылНайден]
